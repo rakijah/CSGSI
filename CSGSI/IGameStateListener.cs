5f7403f1c1a3e5f27ce09a2bb38452d4018ef61e
liamht <ljht93@hotmail.com>
2018-01-26 20:47:56 +0000

    Added game state listener interface. So other people's usages can be unit tested with mocking and DI

---
+++ CSGSI/IGameStateListener.cs
@@ -0,0 +1,25 @@
+﻿using CSGSI.Events;
+
+namespace CSGSI
+{
+    public interface IGameStateListener
+    {
+        GameState CurrentGameState { get; }
+        bool EnableRaisingIntricateEvents { get; set; }
+        int Port { get; }
+        bool Running { get; }
+
+        event BombDefusedHandler BombDefused;
+        event BombExplodedHandler BombExploded;
+        event BombPlantedHandler BombPlanted;
+        event NewGameStateHandler NewGameState;
+        event PlayerFlashedHandler PlayerFlashed;
+        event PlayerGotKillHandler PlayerGotKill;
+        event RoundBeginHandler RoundBegin;
+        event RoundEndHandler RoundEnd;
+        event RoundPhaseChangedHandler RoundPhaseChanged;
+
+        bool Start();
+        void Stop();
+    }
+}
